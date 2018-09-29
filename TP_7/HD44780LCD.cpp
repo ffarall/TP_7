84bc8edf1e2588eaa3f864e8c6ada9b16e0242e4
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-09-29 17:08:08 -0300

    hice lcd move right y left

--- TP_7/HD44780LCD.cpp
+++ TP_7/HD44780LCD.cpp
@@ -56,13 +56,13 @@ bool HD44780LCD::lcdClearToEOL()
 {
 	try
 	{
-		for (int i = (cadd & 0x0F); i <= 16; i++)		// What matters is the lsb of cadd, because any of the lines is being cleared.
+		for (int i = (cadd & 0x0F); i < 16; i++)		// What matters is the lsb of cadd, because any of the lines is being cleared.
 		{
 			handler->lcdWriteDR(SPACE_ASCII);
 		}
 
 		// Now the cursor has to be put back to its original position.
-		if (cadd <= 16)
+		if (cadd < 16)
 		{
 			handler->lcdWriteIR(SET_DDRAM_ADDRESS(cadd));
 		}
@@ -98,14 +98,50 @@ bool HD44780LCD::lcdMoveCursorDown()
 {
 	try
 	{
-		handler->lcdWriteIR(SET_DDRAM_ADDRESS(cadd & 0x4F));		// Cursor goes to position in the first line, in whichever column it was before.
-		lcdUpdateCursor(cadd & 0x2F);
+		handler->lcdWriteIR(SET_DDRAM_ADDRESS(cadd & 0x4F));		// Cursor goes to position in the second line, in whichever column it was before.
+		lcdUpdateCursor(cadd & 0x1F);
 		return true;
 	}
 	catch (ErrType type_)
 	{
 		err.set_type(type_);
 		return false;
+	}
+}
+
+bool HD44780LCD::lcdMoveCursorRight()
+{
+	if (cadd < 32)	// As long as the cursor is not on the last position...
+	{
+		try
+		{
+			handler->lcdWriteIR(SET_DDRAM_ADDRESS(cadd + 1));		
+			lcdUpdateCursor(cadd + 1);
+			return true;
+		}
+		catch (ErrType type_)
+		{
+			err.set_type(type_);
+			return false;
+		}
+	}
+}
+
+bool HD44780LCD::lcdMoveCursorLeft()
+{
+	if (cadd > 0)	// As long as the cursor is not on the fisrt position...
+	{
+		try
+		{
+			handler->lcdWriteIR(SET_DDRAM_ADDRESS(cadd - 1));
+			lcdUpdateCursor(cadd - 1);
+			return true;
+		}
+		catch (ErrType type_)
+		{
+			err.set_type(type_);
+			return false;
+		}
 	}
 }
 
