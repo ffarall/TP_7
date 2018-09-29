199d3f1a2a17905e16cab01b6b32841d4eecf5dc
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-09-29 15:18:46 -0300

    el commit de facu, santi mandale mecha

--- TP_7/FTDIHandler.h
+++ TP_7/FTDIHandler.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <cstdint>
+
 class FTDIHandler
 {
 public:
 	FTDIHandler();
 	~FTDIHandler();
+
+	//Escribe un byte al IR 
+	void lcdWriteIR(uint8_t valor);
+	//Escribe un byte al DR
+	void lcdWriteDR(uint8_t valor); 
 };
 
--- TP_7/HD44780LCD.cpp
+++ TP_7/HD44780LCD.cpp
@@ -1,22 +1,54 @@
 #include "HD44780LCD.h"
+#include "ErrLCD.h"
 
-
+#define CLEAR_DISPLAY 0x01
 
 HD44780LCD::HD44780LCD(): err()
 {
 	try
 	{
 		handler = new FTDIHandler();
+		err.set_type(ErrType::LCD_NO_ERROR);
+		err.update_description();
 	}
 	catch (ErrType type_)
 	{
 		err.set_type(type_);
 		err.update_description();
 	}
+
+
 }
 
 
 HD44780LCD::~HD44780LCD()
 {
 	delete handler;
+}
+
+bool HD44780LCD::lcdInitOk()
+{
+	if (err.get_type() == ErrType::LCD_NO_ERROR)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+FT_STATUS HD44780LCD::lcdGetError()
+{
+	return err.get_status();
+}
+
+bool HD44780LCD::lcdClear()
+{
+	handler->lcdWriteIR(CLEAR_DISPLAY);
+}
+
+bool HD44780LCD::lcdClearToEOL()
+{
+	return false;
 }
